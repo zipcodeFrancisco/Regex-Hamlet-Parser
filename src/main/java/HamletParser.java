20541b77ab52f93bdac4a79d4d6596eca2b124d5
zipcodeFrancisco <zipcodefrancisco@gmail.com>
2019-03-18 01:19:53 -0400

    Completed and Tested

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -36,4 +39,40 @@ public class HamletParser {
         return hamletData;
     }
 
+
+    public String changeOneWord(String wordSource, String wordDestiny, String textSource){
+
+        String patternString = wordSource;
+
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(textSource);
+
+        return matcher.replaceAll(wordDestiny);
+    }
+
+    // TODO Revisar
+    public StringBuilder findHoratio(String textSource){
+        return constructorForFind("(Horatio)", textSource);
+    }
+
+
+    public StringBuilder findHamlet(String textSource) {
+        return constructorForFind("(Hamlet)", textSource);
+    }
+
+    public StringBuilder constructorForFind(String word, String textSource){
+
+        Pattern pattern = Pattern.compile(word);
+        Matcher matcher = pattern.matcher(textSource);
+
+        StringBuilder sb1 = new StringBuilder("");
+
+        while(matcher.find()){
+            sb1.append(matcher.group(1) + " found at "
+                    + matcher.start() + "\n");
+        }
+        return sb1;
+
+    }
 }
+
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,17 +16,64 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        // Given
+        HamletParser hamletParser = new HamletParser();
+        String expected = hamletParser.getHamletData().replaceAll("Hamlet","Leon");
+        // When
+        String actual = hamletParser.changeOneWord("(Hamlet)","Leon",hamletParser.getHamletData());
+        // Then
+        Assert.assertEquals(expected,actual);
+
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        // Given
+        HamletParser hamletParser = new HamletParser();
+        String expected = hamletParser.getHamletData().replaceAll("Hamlet","Leon");
+        expected = expected.replaceAll("Horatio", "Tariq");
+
+        String actual = hamletParser.changeOneWord("(Hamlet)","Leon",hamletParser.getHamletData());
+        actual = hamletParser.changeOneWord("(Horatio)","Tariq",actual);
+
+        Assert.assertEquals(expected,actual);
+
     }
 
     @Test
     public void testFindHoratio() {
+        HamletParser hamletParser = new HamletParser();
+        hamletParser.findHoratio(hamletParser.getHamletData());
+
+        StringBuilder actual = hamletParser.findHoratio(hamletText);
+        StringBuilder expected = new StringBuilder("");
+
+        int index = hamletText.indexOf("Horatio");
+        expected.append("Horatio found at " + index + "\n");
+        while (index >= 0) {
+            index = hamletText.indexOf("Horatio", index + 1);
+            if(index == -1){ break;}
+            expected.append("Horatio found at " + index  + "\n");
+        }
+        Assert.assertEquals(expected,actual);
     }
 
     @Test
     public void testFindHamlet() {
+        HamletParser hamletParser = new HamletParser();
+        hamletParser.findHoratio(hamletParser.getHamletData());
+
+        StringBuilder actual = hamletParser.findHamlet(hamletText);
+        StringBuilder expected = new StringBuilder("");
+
+        int index = hamletText.indexOf("Hamlet");
+        expected.append("Hamlet found at " + index + "\n");
+        while (index >= 0) {
+            index = hamletText.indexOf("Hamlet", index + 1);
+            if(index == -1){ break;}
+            expected.append("Hamlet found at " + index  + "\n");
+        }
+        Assert.assertEquals(expected,actual);
     }
+
 }
